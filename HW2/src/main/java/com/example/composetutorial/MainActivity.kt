4c1372f0606294895f7d3c710ebc86b679983f41
Teirizal <156612913+Teirizal@users.noreply.github.com>
2024-01-29 19:02:55 +0000

    Add files via upload

---
+++ HW2/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -0,0 +1,221 @@
+    package com.example.composetutorial
+
+    import android.os.Bundle
+    import androidx.activity.ComponentActivity
+    import androidx.activity.compose.setContent
+    import androidx.compose.foundation.Image
+    import androidx.compose.foundation.layout.*
+    import androidx.compose.material3.*
+    import androidx.compose.runtime.Composable
+    import androidx.compose.ui.res.painterResource
+    import androidx.compose.ui.Modifier
+    import androidx.compose.ui.tooling.preview.Preview
+    import com.example.composetutorial.ui.theme.ComposeTutorialTheme
+    import androidx.compose.foundation.layout.Spacer
+    import androidx.compose.foundation.layout.height
+    import androidx.compose.foundation.layout.padding
+    import androidx.compose.foundation.layout.size
+    import androidx.compose.foundation.layout.width
+    import androidx.compose.foundation.shape.CircleShape
+    import androidx.compose.ui.draw.clip
+    import androidx.compose.ui.unit.dp
+    import androidx.compose.foundation.border
+    import android.content.res.Configuration
+    import androidx.compose.foundation.lazy.LazyColumn
+    import androidx.compose.foundation.lazy.items
+    import androidx.compose.foundation.clickable
+    import androidx.compose.runtime.getValue
+    import androidx.compose.runtime.mutableStateOf
+    import androidx.compose.runtime.remember
+    import androidx.compose.runtime.setValue
+    import androidx.compose.animation.animateColorAsState
+    import androidx.compose.animation.animateContentSize
+    import androidx.compose.material3.MaterialTheme
+    import androidx.compose.material3.Surface
+    import androidx.activity.compose.BackHandler
+    import androidx.compose.material.icons.Icons
+    import androidx.compose.material.icons.filled.Settings
+    import androidx.compose.material3.ExperimentalMaterial3Api
+    import androidx.compose.material3.IconButton
+    import androidx.compose.material3.Text
+    import androidx.compose.material3.TopAppBar
+    import androidx.compose.material.icons.filled.ArrowBack
+
+
+    enum class ViewType { VIEW_A, VIEW_B }
+
+    class MainActivity : ComponentActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContent {
+                //Text("Hello world!")
+                /*            ComposeTutorialTheme {
+                    // A surface container using the 'background' color from the theme
+                    Conversation(SampleData.conversationSample)
+                    Surface(modifier = Modifier.fillMaxSize()) { //, color = MaterialTheme.colorScheme.background) {
+                        MessageCard(Message("Android", "Jetpack Compose"))
+                        //Greeting("Android")*/
+                ComposeTutorialTheme {
+                    var currentView by remember { mutableStateOf(ViewType.VIEW_B) }
+                    val messages = SampleData.conversationSample
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        when (currentView) {
+                            ViewType.VIEW_A -> ViewA(onNavigateToViewB = { currentView = ViewType.VIEW_B })
+                            ViewType.VIEW_B -> ViewB(onNavigateToViewA = { currentView = ViewType.VIEW_A }, messages = messages)
+                        }
+                        BackHandler {
+                            if (currentView == ViewType.VIEW_A) {
+                                currentView = ViewType.VIEW_B
+                            } else {
+                                finish()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun ViewA(onNavigateToViewB: () -> Unit) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            TopAppBar(
+                title = { Text("Settings", Modifier.weight(1f)) },
+                navigationIcon = {
+                    IconButton(onClick = onNavigateToViewB) {
+                        Icon(Icons.Filled.ArrowBack, contentDescription = "Go back")
+                    }
+                }
+            )
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun ViewB(onNavigateToViewA: () -> Unit, messages: List<Message>) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            TopAppBar(
+                title = { Text("Messages") },
+                actions = {
+                    IconButton(onClick = onNavigateToViewA) {
+                        Icon(Icons.Filled.Settings, contentDescription = null)
+                    }
+                }
+            )
+            Conversation(messages)
+        }
+    }
+
+    data class Message(val author: String, val body: String)
+
+    @Composable
+    fun MessageCard(msg: Message) {
+        // Add padding around our message
+        Row (modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(R.drawable.profile_picture),
+                contentDescription = null, //"Contact profile picture",
+                modifier = Modifier
+                    // Set image size to 40 dp
+                    .size(40.dp)
+                    // Clip image to be shaped as a circle
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+            )
+
+            // Add a horizontal space between the image and the column
+            Spacer(modifier = Modifier.width(8.dp))
+
+            // We keep track if the message is expanded or not in this
+            // variable
+            var isExpanded by remember { mutableStateOf(false) }
+            // surfaceColor will be updated gradually from one color to the other
+            val surfaceColor by animateColorAsState(
+                if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+            )
+
+
+            // We toggle the isExpanded variable when we click on this Column
+            Column(modifier = Modifier.clickable { isExpanded = !isExpanded })  {
+                Text(
+                    text = msg.author,
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.titleSmall
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    shadowElevation = 1.dp,
+                    // surfaceColor color will be changing gradually from primary to surface
+                    color = surfaceColor,
+                    // animateContentSize will change the Surface size gradually
+                    modifier = Modifier.animateContentSize().padding(1.dp)
+                ) {
+                    Text(
+                        text = msg.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        // If the message is expanded, we display all its content
+                        // otherwise we only display the first line
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+            }
+        }
+    }
+
+    @Preview(name = "Light Mode")
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
+
+    @Composable
+    fun PreviewMessageCard() {
+        ComposeTutorialTheme {
+            Surface {
+                MessageCard(
+                    msg = Message("Lexi","Hey, take a look at Jetpack Compose, it's great!")
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun Conversation(messages: List<Message>) {
+        LazyColumn {
+            items(messages) { message ->
+                MessageCard(message)
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewConversation() {
+        ComposeTutorialTheme {
+            Conversation(SampleData.conversationSample)
+        }
+    }
+
+
+    /*
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        ComposeTutorialTheme {
+            Greeting("Android")
+        }
+    }*/
